41fda95af4bad92049fb2674d9fec1a9299c1411
Igor Štikovac <igorsti@telekom.rs>
2026-05-18 10:08:34 +0200

    Prvi commit

---
+++ DAY3/job02.py
@@ -0,0 +1,5 @@
+number_to_check = int(input("Enter a number to check if it's even or odd: "))
+if number_to_check % 2:
+    print(f"{number_to_check} is an odd number.")
+else:
+    print(f"{number_to_check} is an even number.")
